9f9606a3574747edbbc2c998f59c1a1c07493448
viktor.podzigun@gmail.com <viktor.podzigun@gmail.com@c37bb64c-2652-77d2-3aae-87cdcc7544ec>
2014-08-27 15:47:27 +0000

    [showcase] Added configuration texts editing to config panel

--- web-common-showcase/src/main/java/com/googlecode/common/showcase/client/widgets/ConfigPanelView.java
+++ web-common-showcase/src/main/java/com/googlecode/common/showcase/client/widgets/ConfigPanelView.java
@@ -1,93 +1,105 @@
 
 package com.googlecode.common.showcase.client.widgets;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import org.fusesource.restygwt.client.MethodCallback;
-import org.fusesource.restygwt.client.RestService;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ExternalTextResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.googlecode.common.client.app.AppMainPanel;
-import com.googlecode.common.client.app.task.RequestTask;
 import com.googlecode.common.client.config.ui.ConfigEditPanel;
-import com.googlecode.common.client.http.RequestService;
+import com.googlecode.common.client.task.AbstractTextTask;
 import com.googlecode.common.client.task.TaskManager;
+import com.googlecode.common.client.ui.ButtonImages;
 import com.googlecode.common.client.ui.ImageButton;
-import com.googlecode.common.client.ui.TextField;
-import com.googlecode.common.protocol.DataResponse;
+import com.googlecode.common.client.ui.TextAreaField;
+import com.googlecode.common.client.ui.panel.LoadablePanel;
 
 
-public final class ConfigPanelView extends Composite {
+public final class ConfigPanelView extends LoadablePanel {
 
     private static final Binder binder = GWT.create(Binder.class);
     interface Binder extends UiBinder<Widget, ConfigPanelView> {
     }
 
-    @UiField TextField      textName;
-    @UiField TextField      textId;
-    @UiField ImageButton    btnConfig;
+    @UiField ImageButton        btnConfig;
+    @UiField TextAreaField      textSchema;
+    @UiField TextAreaField      textData;
     
-    private final AppMainPanel mainPanel;
+    private final AppMainPanel  mainPanel;
     
     
     public ConfigPanelView(AppMainPanel mainPanel, String name) {
         this.mainPanel = mainPanel;
         
         initWidget(binder.createAndBindUi(this));
-    
-        textName.setText(name);
-        textName.setReadOnly(true);
         
-        textId.setText("67071c0a-f862-4799-8893-29e08ac40940");
-        textId.setReadOnly(true);
+        btnConfig.setImage(ButtonImages.INSTANCE.edit());
+    }
+    
+    @Override
+    public void onDeactivated() {
+        setNeedLoad(false);
+    }
+    
+    @Override
+    public void onLoadData() {
+        TaskManager.INSTANCE.execute(new LoadSchemaTask(
+                JsonResources.INSTANCE.configSchemaJson()));
+        
+        TaskManager.INSTANCE.execute(new LoadDataTask(
+                JsonResources.INSTANCE.configDataJson()));
     }
     
     @UiHandler("btnConfig")
     void onBtnConfigClick(ClickEvent event) {
-        TaskManager.INSTANCE.execute(new LoadConfigTask());
-    }
-    
-    private void showConfigPanel(JSONValue resp) {
         ConfigEditPanel dlg = new ConfigEditPanel(mainPanel, 
-                textName.getText() + " configuration", resp);
+                "Edit configuration", 
+                JSONParser.parseStrict(textSchema.getText().trim()).isObject(), 
+                JSONParser.parseStrict(textData.getText().trim()).isObject());
         dlg.show();
     }
     
+    
+    interface JsonResources extends ClientBundle {
+        
+        static final JsonResources INSTANCE = GWT.create(JsonResources.class);
 
-    /**
-     * Configuration service interface declaration.
-     */
-    interface ConfigService extends RestService {
-
-        @GET
-        @Path("/config")
-        public void getTestData(MethodCallback<DataResponse<JSONValue>> callback);
-
+        @Source("config.data.json")
+        ExternalTextResource configDataJson();
+    
+        @Source("config.schema.json")
+        ExternalTextResource configSchemaJson();
     }
     
     
-    private class LoadConfigTask extends RequestTask<DataResponse<JSONValue>> {
+    private class LoadSchemaTask extends AbstractTextTask {
         
-        public LoadConfigTask() {
-            super("Loading data...");
+        public LoadSchemaTask(ExternalTextResource res) {
+            super("Loading config schema...", res);
         }
         
         @Override
-        protected void runTask() throws Exception {
-            ConfigService service = GWT.create(ConfigService.class);
-            RequestService.prepare(service).getTestData(this);
-        }
-        
-        @Override
-        protected void processSuccessResponse(DataResponse<JSONValue> resp) {
-            showConfigPanel(resp.getData());
+        public void processResult(String text) {
+            textSchema.setText(text);
         }
     }
+
     
+    private class LoadDataTask extends AbstractTextTask {
+        
+        public LoadDataTask(ExternalTextResource res) {
+            super("Loading config data...", res);
+        }
+        
+        @Override
+        public void processResult(String text) {
+            textData.setText(text);
+        }
+    }
+
 }
